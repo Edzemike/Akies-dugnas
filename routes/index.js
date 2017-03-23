f7596c00effe465feb09daeff357713732b17db3
martintubis <martin.t1995@gmail.com>
2017-03-23 16:54:24 +0200

    Added file upload

--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,35 @@ var express = require('express');
 var router = express.Router();
 var csrf = require('csurf');
 var passport = require('passport');
+var multer = require('multer');
+var upload = multer({dest: 'public/uploads/'})
 
 var csrfProtection = csrf();
 router.use(csrfProtection);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('eyeExamination', { title: 'Express' });
+    res.render('eyeExamination/index', { csrfToken: req.csrfToken() });
+});
+
+router.post('/', upload.any(), function(req, res, next) {
+    res.send(req.files);
 });
 
 router.get('/user/signup', function(req, res, next){
-  res.render('user/signup', {csrfToken: req.csrfToken()})
+    res.render('user/signup', {csrfToken: req.csrfToken()})
 });
 
 router.post('/user/signup', passport.authenticate('local.signup', {
-      successRedirect: '/',
-      failureRedirect: '/user/signup',
-      failureFlash: true
+    successRedirect: '/',
+    failureRedirect: '/user/signup',
+    failureFlash: true
 }));
 
 router.get('/user/profile', function(req, res, next){
-  res.render('user/profile');
+    res.render('user/profile');
 });
+
+
 module.exports = router;
+
